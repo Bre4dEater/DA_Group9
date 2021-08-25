04e670a3ca369b65714f8d8dbbe48226b8d2fe8f
Memelord <Memelord@DESKTOP-SH257I8>
2021-08-26 01:38:16 +0800

    Add project files.

---
+++ PythonApplication5/PythonApplication5.py
@@ -0,0 +1,36 @@
+import scrapy
+import requests
+
+url = 'https://brickset.com/sets/year-2006'
+h = requests.head(url)
+print("Header:")
+print("**********")
+r = requests.get('https://brickset.com/sets/year-2006')
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+headers = {'User-Agent' : 'Iphone 8'}
+# Test it on an external site
+url2 = 'http://httpbin.org/headers'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
+
+class NewSpider(scrapy.Spider):
+    name = "new_spider"
+    start_urls = ['https://books.toscrape.com/']
+
+    def parse(self, response):
+        css_selector = 'img'
+
+        for x in response.css(css_selector):
+            newsel = '@src'
+            yield {
+                'Image Link': x.xpath(newsel).extract_first(),
+            }
+            Page_selector = '.next a ::attr(href)'
+            next_page = response.css(Page_selector).extract_first()
+            if next_page:
+                yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
+                )
