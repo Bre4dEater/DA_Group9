9c202dbdaf3beac1ccd18cbdd8b6684f6beff225
Bread <limignatius03@gmail.com>
2021-08-26 12:53:07 +0800

    test

--- PythonApplication5/PythonApplication5.py
+++ PythonApplication5/PythonApplication5.py
@@ -3,6 +3,8 @@ import requests
 import unittest
 url = 'https://brickset.com/sets/year-2006'
 h = requests.head(url)
+print("Status code:")
+print("\t *", h.status_code)
 print("Header:")
 print("**********")
 r = requests.get('https://brickset.com/sets/year-2006')
@@ -14,8 +16,7 @@ headers = {'User-Agent' : 'Mobile'}
 url2 = 'http://httpbin.org/headers'
 rh = requests.get(url2, headers=headers)
 print(rh.text)
-print("Status code:")
-print("\t *", h.status_code)
+
 class Test(unittest.TestCase):
     def test1(self):
         self.assertEqual(h.status_code, 200)
